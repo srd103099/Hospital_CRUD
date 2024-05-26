86f0d2d1e5f07a6244520609e9c9fdeef441ab62
JulianSuarez5 <Juacosuarez12@gmail.com>
2024-05-26 14:07:37 -0500

    New Changes

--- Hospital_CRUD/Presentacion/frmServicios.cs
+++ Hospital_CRUD/Presentacion/frmServicios.cs
@@ -1,7 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel;
+using System.Configuration;
 using System.Data;
+using System.Data.SqlClient;
 using System.Drawing;
 using System.Linq;
 using System.Text;
@@ -26,7 +28,39 @@ namespace Hospital_CRUD
 
         private void btnBuscar_Click(object sender, EventArgs e)
         {
+            // Cadena de conexión a la base de datos
+            string cadenaConexion = ConfigurationManager.ConnectionStrings["PROYECTO_FINAL3"].ConnectionString;
 
+            // Consulta SQL para obtener todos los servicios
+            string consultaSQL = "SELECT * FROM SERVICIOS";
+
+            // Crear una nueva instancia de SqlConnection
+            using (SqlConnection conexion = new SqlConnection(cadenaConexion))
+            {
+                // Crear un adaptador de datos SQL para ejecutar la consulta
+                using (SqlDataAdapter adaptador = new SqlDataAdapter(consultaSQL, conexion))
+                {
+                    // Crear un DataTable para almacenar los resultados
+                    DataTable tablaServicios = new DataTable();
+
+                    try
+                    {
+                        // Abrir la conexión a la base de datos
+                        conexion.Open();
+
+                        // Llenar el DataTable con los resultados de la consulta
+                        adaptador.Fill(tablaServicios);
+
+                        // Asignar el DataTable como el DataSource del DataGridView
+                        dgvServicios.DataSource = tablaServicios;
+                    }
+                    catch (Exception ex)
+                    {
+                        // Mostrar un mensaje de error si algo sale mal
+                        MessageBox.Show("Error al obtener los servicios: " + ex.Message);
+                    }
+                }
+            }
         }
     }
 }
